9f88bde8b8e1f59c5947775839a3c1b4ab121fc0
LIUST3 <stefan.liu@oocl.com>
2019-07-25 10:05:18 +0800

    [stefan] init: package manage page

---
+++ src/store/actions.js
@@ -0,0 +1,43 @@
+// import Vue from 'vue'
+// import axios from 'axios'
+// import VueAxios from 'vue-axios'
+
+// Vue.use(VueAxios, axios);
+const action = {
+  // getTodos({ commit }) {
+  //   Vue.axios
+  //     .get("http://5d366dcf86300e0014b641f1.mockapi.io/api/v1/todolist")
+  //     .then((response) => {
+  //       commit('setTodoList', { list: response.data })
+  //     }).catch(error => console.log(error))
+  // },
+  // addTodo(context, data) {
+  //   Vue.axios
+  //     .post("http://5d366dcf86300e0014b641f1.mockapi.io/api/v1/todolist", data)
+  //     .then((response)=>{
+  //       if (response.status === 201) {
+  //         context.dispatch('getTodos')
+  //       }
+  //     }).catch(error => console.log(error))
+  // },
+  // updateTodo(context , data) {
+  //   Vue.axios
+  //     .put(`http://5d366dcf86300e0014b641f1.mockapi.io/api/v1//todolist/${data.id}`, data)
+  //     .then((response)=>{
+  //       if (response.status === 200) {
+  //         context.dispatch('getTodos');
+  //       }
+  //     }).catch(error => console.log(error))
+  // },
+  // deleteTodo(context, data) {
+  //   Vue.axios
+  //     .delete(`http://5d366dcf86300e0014b641f1.mockapi.io/api/v1//todolist/${data.id}`)
+  //     .then((response)=>{
+  //       if (response.status === 200) {
+  //         context.dispatch('getTodos');
+  //       }
+  //     }).catch(error => console.log(error))
+  // }
+}
+
+export default action;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <a-layout>
+    <a-layout-header></a-layout-header>
+    <a-layout>
+      <a-layout-sider width="200" style="background: #fff">
+        <a-menu
+          mode="inline"
+          :defaultSelectedKeys="['1']"
+          :style="{ height: '100%', borderRight: 0 }"
+        >
+          <a-menu-item key="1">option1</a-menu-item>
+          <a-menu-item key="2">option2</a-menu-item>
+          <a-menu-item key="3">option3</a-menu-item>
+          <a-menu-item key="4">option4</a-menu-item>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout style="padding: 0 24px 24px">
+        <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>Home</a-breadcrumb-item>
+          <a-breadcrumb-item>List</a-breadcrumb-item>
+        </a-breadcrumb>
+        <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
+        <router-view/>
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+    <a-layout-content></a-layout-content>
+    <a-layout-footer></a-layout-footer>
+  </a-layout>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home"
+};
 </script>
